ffa7b957a1f40d492e8b4b20b900809aaed3a315
Ted Ooghe <tedaeo@gmail.com>
2020-08-14 00:16:05 -0700

    age check, invite

--- firstgame.py
+++ firstgame.py
@@ -3,3 +3,11 @@ name = input('What is your name? ')
 age = input('How old are you? ')
 
 print('Hello,', name, '.  You are', age, 'years old.')
+
+if age >= 18:
+  print('You are old enough.')
+  wantplay = input('Do you want to play? ')
+  if wantplay == 'yes':
+    print("let's play!")
+else:
+  print('Sorry, you are too young to play.')
